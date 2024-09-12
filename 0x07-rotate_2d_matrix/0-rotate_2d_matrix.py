70e60f6b70404a94b035e294d74342e36d83a28a
Areejslama <areegahmed449@gmail.com>
2024-09-13 01:07:28 +0300

    0

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -9,20 +9,9 @@ def rotate_2d_matrix(matrix):
         first, last = layer, n - layer - 1
 
         for i in range(first, last):
-            # offset keeps track of how far the element is from the 'first'
             offset = i - first
-            
-            # Save the top element
             top = matrix[first][i]
-            
-            # Move left to top
             matrix[first][i] = matrix[last - offset][first]
-            
-            # Move bottom to left
             matrix[last - offset][first] = matrix[last][last - offset]
-            
-            # Move right to bottom
             matrix[last][last - offset] = matrix[i][last]
-            
-            # Move top to right
             matrix[i][last] = top
