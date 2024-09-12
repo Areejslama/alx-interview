d69a8b51d63a310f71bfdef544eca667d8fbb8e6
Areejslama <areegahmed449@gmail.com>
2024-09-13 00:41:51 +0300

    0

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -4,9 +4,25 @@
 
 def rotate_2d_matrix(matrix):
     """define the 2D matrix"""
-    n = len(matrix)
-    for row in matrix:
-        row.reverse()
-    for i in range(n):
-        for j in range(i + 1, n):
-            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    N = len(matrix)  # Get the size of the matrix dynamically
+
+    # Consider all squares one by one
+    for x in range(0, N // 2):
+        
+        # Consider elements in groups of 4 in the current square
+        for y in range(x, N - x - 1):
+            
+            # Store the current cell in a temporary variable
+            temp = matrix[x][y]
+
+            # Move values from right to top
+            matrix[x][y] = matrix[y][N - 1 - x]
+
+            # Move values from bottom to right
+            matrix[y][N - 1 - x] = matrix[N - 1 - x][N - 1 - y]
+
+            # Move values from left to bottom
+            matrix[N - 1 - x][N - 1 - y] = matrix[N - 1 - y][x]
+
+            # Assign the temp to the left
+            matrix[N - 1 - y][x] = temp
