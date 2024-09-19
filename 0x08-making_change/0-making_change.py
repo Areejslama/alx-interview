3eff8ef09056c2acba3dabe42fe7631dabe0a723
Areejslama <areegahmed449@gmail.com>
2024-09-19 23:43:10 +0300

    coins

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,11 +4,22 @@
 
 def makeChange(coins, total):
     """define function"""
-    if total == 0:
+    if coins is None:
+        return -1
+    if total <= 0:
         return 0
-    if total < 0:
-        return float('inf')
-    if len(coins) == 0:
-         return float('inf')
-    return min(makeChange(coins[1:], total),
-            makeChange(coins, total - coins[0]) + 1)
+    change = 0
+    coins = sorted(coins, reverse=True)
+
+    i = 0
+    while total > 0 and i < len(coins):
+        coin = coins[i]
+        while coin <= total:
+            total -= coin
+            change += 1
+        i += 1
+
+    if total == 0:
+        return change
+    else:
+        return -1
