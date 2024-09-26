65cca8e4a0f4ea022a79a0412337df657b34c407
Areejslama <areegahmed449@gmail.com>
2024-09-27 00:05:28 +0300

    land

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -7,22 +7,21 @@ def island_perimeter(grid):
     perimeter = 0
     m = len(grid)
     n = len(grid[0])
-    
+
     for i in range(m):
         for j in range(n):
-            
+
             if grid[i][j] == 1:
-                
+
                 if i == 0 or grid[i-1][j] == 0:
                     perimeter += 1
-                        
+
                 if i == m-1 or grid[i+1][j] == 0:
                     perimeter += 1
-                    
+
                 if j == 0 or grid[i][j-1] == 0:
                     perimeter += 1
-                    
+
                 if j == n-1 or grid[i][j+1] == 0:
                     perimeter += 1
-                    
     return perimeter
