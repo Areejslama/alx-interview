e43ce1b823b7c9e503f2e4cd94f580d536b00770
Areejslama <areegahmed449@gmail.com>
2024-10-02 02:12:09 +0300

    win

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,33 +3,30 @@
 
 
 def isWinner(x, nums):
-    """define function"""
+    """Determine the winner of the game for x rounds."""
     if x <= 0 or not nums:
         return None
     max_nums = max(nums)
-    prime = [True for i in range(max_nums+1)]
+    prime = [True for i in range(max_nums + 1)]
     prime[0], prime[1] = False, False
     p = 2
     while (p * p <= max_nums):
-
         if prime[p]:
-            for i in range(p * p, max_nums+1, p):
+            for i in range(p * p, max_nums + 1, p):
                 prime[i] = False
         p += 1
+    maria_wins = 0
+    ben_wins = 0
 
-        maria_wins = 0
-        ben_wins = 0
-
-        for n in nums:
-            count = sum(prime[2:n+1])
-        if count % 2:
+    for n in nums:
+        count = sum(prime[2:n+1])
+        if count % 2 == 1:
             maria_wins += 1
         else:
             ben_wins += 1
-
-        if maria_wins > ben_wins:
-            return "maria"
-        elif maria_wins < ben_wins:
-            return "ben"
-        else:
-            return None
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return None
