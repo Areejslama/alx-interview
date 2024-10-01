f77e38dcd5192c22d57ae0ce53544b70af366df2
Areejslama <areegahmed449@gmail.com>
2024-10-02 02:07:49 +0300

    win

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""this script to define method"""
+
+
+def isWinner(x, nums):
+    """define function"""
+    if x <= 0 or not nums:
+        return None
+    max_nums = max(nums)
+    prime = [True for i in range(max_nums+1)]
+    prime[0], prime[1] = False, False
+    p = 2
+    while (p * p <= max_nums):
+
+        if prime[p]:
+            for i in range(p * p, max_nums+1, p):
+                prime[i] = False
+        p += 1
+
+        maria_wins = 0
+        ben_wins = 0
+
+        for n in nums:
+            count = sum(prime[2:n+1])
+        if count % 2:
+            maria_wins += 1
+        else:
+            ben_wins += 1
+
+        if maria_wins > ben_wins:
+            return "maria"
+        elif maria_wins < ben_wins:
+            return "ben"
+        else:
+            return None
